6fbf5b1faf38facedd246472b88497e8c3c9dbb7
timmytimeless_cp <SND\timmytimeless_cp>
2011-02-28 05:28:14 +0000

    Change Log altered, Assembly Info updated

--- XmlToCsvConverter/XmlToCsvConverter/Properties/AssemblyInfo.cs
+++ XmlToCsvConverter/XmlToCsvConverter/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("XmlToCsvConverter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("GUI to convert XML data to CSV data")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MOOR Software Development")]
+[assembly: AssemblyCompany("Moor Software Development")]
 [assembly: AssemblyProduct("XmlToCsvConverter")]
-[assembly: AssemblyCopyright("Copyright © MOOR Software Development 2008")]
+[assembly: AssemblyCopyright("Copyright © Moor Software Development 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
